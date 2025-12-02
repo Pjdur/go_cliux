88489627b41e83462eb4525b498120cfa2529080
Pjdur <pjdurable@gmail.com>
2025-12-02 20:48:44 +0000

    Initail commit

---
+++ app.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"strings"
+)
+
+func main() {
+	Boxed("Google made Go", "Cliux", 40)
+	Label("Cliux", "success")
+	Label("Cliux", "error")
+	Label("Cliux", "info")
+	Section("Cliux", "Go is made by Google", 30)
+}
+
+func Boxed(text string, name string, width int) {
+	text = strings.TrimSpace(text)
+	name = strings.TrimSpace(name)
+
+	boxWidth := width
+	fmt.Println(strings.Repeat("-", boxWidth))
+	fmt.Println("¦  ", name, strings.Repeat(" ", boxWidth-len(name)-9), "  ¦")
+	fmt.Println(strings.Repeat("-", boxWidth))
+	fmt.Println("¦  ", text, strings.Repeat(" ", boxWidth-len(text)-9), "  ¦")
+	fmt.Println(strings.Repeat("-", boxWidth))
+}
+
+func Label(content string, style string) {
+	switch style {
+	case "info":
+		color.Blue(content)
+	case "error":
+		color.Red(content)
+	case "success":
+		color.Green(content)
+	}
+}
+
+func Section(content string, name string, width int) {
+	fmt.Println(name)
+	fmt.Println(strings.Repeat("-", width))
+	fmt.Println(content)
+}
